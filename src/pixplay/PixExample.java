01d977d4b17b401a1e3c3cbbc223be7bc523013e
Boxmein <boxmein@boxmein.web44.net>
2012-08-04 22:40:10 +0300

    Added circle drawing and a new move() function for PixExample. Fixed a lot of stuff and added pausing and resizing. (Pause: SPACEBAR, resize brush: UP/DOWN ARROW)

--- src/pixplay/PixExample.java
+++ src/pixplay/PixExample.java
@@ -1,8 +1,21 @@
 package pixplay;
-
+/* Element reference: 
+ * 0x0: Empty
+ * 0x01: PixExample
+ * 
+ *  
+ *  
+ *  */
 public class PixExample {
   public static int colour = 0xFF0000; 
   public static int update(int x, int y) {
+    if ( x > 398 || x < 2 || y < 2 || y > 398) 
+     {
+    	return 1;
+     }
+      return 0; //Success
+    }
+  public static int move(int x, int y) {
     if ( x > 398 || x < 2 || y < 2 || y > 398) 
      {
     	PixPlay.pixel[x][y] = 0x0;
@@ -12,6 +25,6 @@ public class PixExample {
        PixPlay.pixel[x-1][y] = 0x01;
       PixPlay.pixel[x][y] = 0x0;
       return 0; //Success
-    }
+  }
 }
 
--- src/pixplay/PixPlay.java
+++ src/pixplay/PixPlay.java
@@ -8,7 +8,7 @@ public class PixPlay extends PApplet {
 	 */
 	private static final long serialVersionUID = -3911587658158706430L;
 	public static byte[][] pixel;
-	boolean drawing = false;
+	boolean drawing = false, paused = false;
 	byte selected = 0x1; // PixExample
 	short brush = 1;
 	public void setup () {
@@ -17,22 +17,33 @@ public class PixPlay extends PApplet {
 	  pixel = new byte[width][height];
 	  pixel[100][100] = 0x01;
 	  background(0xFFFFFF);
+	  ellipseMode(CENTER); // Draws cursor to the right place
+	  noCursor(); // Start with no cursor
 	  
 	}
 
 	public void draw() {
+	 
 	  //background(0xFF00FF);
-      //Drawing block
 	  background(0xFFFFFF);
+      //Drawing block
+	  //int xPos, yPos;
 	  if (drawing && 
 			  mouseX < width && 
 			  mouseX > 0 && 
 			  mouseY < 400 && 
 			  mouseY > 0) {
 		  
-		  if(pixel[mouseX][mouseY] == 0x0)
-		  pixel[mouseX][mouseY] = selected;
+		  if (brush > 1) {
+			  drawCircle(mouseX, mouseY);
+		  }
+		  else {
+			  if(pixel[mouseX][mouseY] == 0x0)
+				  pixel[mouseX][mouseY] = selected;
+		  }
 	  }
+	  //Visuals block
+	  ellipse(mouseX, mouseY, brush, brush); // Cursor circle
 	  
 	  //Particle update block
 	  int x=0, y=0;
@@ -42,21 +53,26 @@ public class PixPlay extends PApplet {
 	        
 	        switch (pixel[x][y]) { // Converts signed byte to unsigned
 	          
-	          case 0x01: //Example player
+	          case 0x01: //PixExample
 	        	//println("updating 0x01 with x: "+x+" and y: "+y);
 	            fill(PixExample.colour);
 	            point(x, y);
 	            //println("update returned: "+);  
-	            PixExample.update(x, y);
+	            if (!paused) {
+	            	PixExample.move(x, y);
+	            	PixExample.update(x, y);
+	            	
+	            }
 	            break;
 	            
 	          default: //Default everything
+	        	  fill(0xFFFFFF);
+	        	  point(x, y);
 	            break;
 	        }
 	      }
 	    }
 	  }
-	  
 	}
 	// Mouse events for drawing
 	public void mousePressed () {
@@ -72,8 +88,35 @@ public class PixPlay extends PApplet {
 		else if(keyCode == DOWN) {
 			if ( brush > 1) brush--;
 		}
+		else if(key == ' ') {
+			paused = !paused;
+		}
 		else return;
 		
+		if (paused) { // Turns off cursor when playing
+			cursor(ARROW);
+		}
+		else {
+			noCursor();
+		}
 	}
-
+	public void drawCircle(int x, int y) { // This cryptic function from Java Powder
+		int tempy = y; 
+		for (int i = x - brush; i <= x; i++) {
+			double distance = Math.sqrt(Math.pow((double) x - (double) i, (double) 2) + 
+								Math.pow((double) y - (double) tempy, (double) 2));
+				while (distance <= brush) {
+					tempy = tempy - 1;
+					distance = Math.sqrt(Math.pow((double) x - (double) i, (double) 2) + 
+								Math.pow((double) y - (double) tempy, (double) 2));
+				}
+				tempy = tempy + 1;
+				for (int j = tempy; j <= 2 * y - tempy; j++) {
+					if (i > 0 && j > 0 && i < width && j < height)
+						pixel[i][j] = selected;
+					if (2 * x - i > 0 && j > 0 && 2 * x - i < width && j < height)
+						pixel[2*x - i][j] = selected;
+				}
+		}
+	}
 }
